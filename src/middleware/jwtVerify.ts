8dc6bc562aacfc25714a4532fbf2a360d2dd18f3
Alexander Petkov <sashko506@gmail.com>
2024-04-25 20:26:04 +0300

    chore(jwtVerify): Remove unused import

--- src/middleware/jwtVerify.ts
+++ src/middleware/jwtVerify.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, Application } from "express";
+import { Request, Response, NextFunction } from "express";
 
 import * as jwt from "jsonwebtoken";
 import { env } from "../../config";
